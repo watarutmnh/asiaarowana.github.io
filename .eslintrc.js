4887167cafc9976ba3867d15b16cf18f972acbbd
watarutamanaha <wataru@cdgrph.com>
2017-07-20 02:33:26 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "standard",
+    "plugins": ["standard"],
+    "rules": {
+        "semi": [2, "always"],
+        "indent": ["error", 4],
+        "space-before-function-paren": [2, "never"],
+        "no-unused-vars": 0,
+    },
+    "env": {
+        "es6": true,
+        "jquery": true
+    }
+};
